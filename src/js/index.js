997cb73061d77d62cecdca89a03c3da5e4b2357f
ViniciusBeuther <vinicius.beuther15@gmail.com>
2023-10-13 22:23:41 -0300

    Added SOA and links

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '50px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal('#home_title, .card, .team-img, .div-hover-effect, #main_marketing_message, #sub_marketing_message, .soa-text', { delay: 200 });
+
